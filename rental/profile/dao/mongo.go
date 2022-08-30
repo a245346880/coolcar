fc258156970c019f984e2e1b32a3ae32aa536141
777666 <xiaoyang777666@126.com>
2022-08-30 21:09:58 +0800

    新增rental包mongo操作

---
+++ rental/profile/dao/mongo.go
@@ -0,0 +1,63 @@
+package dao
+
+import (
+	"context"
+	rentalpb "coolcar/rental/api/gen/v1"
+	"coolcar/shared/id"
+	mgutil "coolcar/shared/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	accountIDField      = "accountid"
+	profileField        = "profile"
+	identityStatusField = profileField + ".identitystatus"
+	photoBlobIDField    = "photoblobid"
+)
+
+type Mongo struct {
+	col *mongo.Collection
+}
+
+func NewMongo(db *mongo.Database) *Mongo {
+	return &Mongo{col: db.Collection("profile")}
+}
+
+type ProfileRecord struct {
+	AccountID   string            `bson:"accountid"`
+	Profile     *rentalpb.Profile `bson:"profile"`
+	PhotoBlobID string            `bson:"photoblobid"`
+}
+
+func (m *Mongo) UpdateProFile(c context.Context, aid id.AccountID, prevState rentalpb.IdentityStatus, p *rentalpb.Profile) error {
+	filter := bson.M{
+		identityStatusField: prevState,
+	}
+	if prevState == rentalpb.IdentityStatus_UNSUBMITTED {
+		filter = mgutil.ZeroOrDoesNotExist(identityStatusField, prevState)
+	}
+	filter[accountIDField] = aid.String()
+	_, err := m.col.UpdateOne(c, filter, mgutil.Set(bson.M{
+		accountIDField: aid.String(),
+		profileField:   p,
+	}), options.Update().SetUpsert(true))
+	return err
+}
+
+func (m *Mongo) UpdateProfilePhoto(c context.Context, aid id.AccountID, bid id.BlobID) error {
+	_, err := m.col.UpdateOne(c, bson.M{
+		accountIDField: aid,
+	}, mgutil.Set(bson.M{
+		accountIDField:   aid.String(),
+		photoBlobIDField: bid.String(),
+	}), options.Update().SetUpsert(true))
+	return err
+}
+
+func byAccountID(aid id.AccountID) bson.M {
+	return bson.M{
+		accountIDField: aid.String(),
+	}
+}
